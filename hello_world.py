b730bcef35ca2eb46ea372131361c21d86afab28
KumohANC <seafoo@kumoh.ac.kr>
2022-09-21 17:16:03 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,14 @@
-def print_hello():
-    print("Hello World!!")
+class Test:
+    def __init__(self):
+        pass
 
-def print_rip():
-    print("raip")
+
+    def printMessage(self):
+        print("printMessage")
+
+def main():
+    test = Test()
+    test.printMessage()
 
 if __name__ == '__main__':
-    print_hello()
-    rip()
+    main()
