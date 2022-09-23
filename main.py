be7a6221aea287c16ad00ee21468918aef86c13c
KumohANC <seafoo@kumoh.ac.kr>
2022-09-23 13:13:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ import prime_number as pn
 if __name__ == '__main__':
     pn.is_prime_number(1)
     pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+
 
